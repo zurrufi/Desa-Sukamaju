1c6df322042bdf0bcb0e50e34d211cb2563fc360
zurrufi <144505889+zurrufi@users.noreply.github.com>
2023-10-27 20:55:23 +0700

    Update Banner.vue
    rubah nama header

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -8,8 +8,9 @@
     <div class="ml-10 flex flex-col justify-center">
       <p class="text-white text-2xl mb-2">Profile Desa</p>
       <!-- Sesuaikan nama desa masing-masing -->
-      <p class="text-white text-3xl">Desa Sukamaju</p>
-      <p class="text-white text-3xl">Kab. Bogor</p>
+      <p class="text-white text-3xl">DESA SUKAMAJU</p>
+      <p class="text-white text-3xl">KECAMATAN CIBUNGBULANG</p>
+      <p class="text-white text-3xl">KABUPATEN BOGOR</p>
     </div>
     <div class="grid place-items-center ml-auto mr-20">
       <iframe
