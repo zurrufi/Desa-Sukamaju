46d62976fbaaf651cd97994398a739fdcf66336e
zurrufi <144505889+zurrufi@users.noreply.github.com>
2023-10-27 21:19:43 +0700

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-turquoise-600 h-[300px] flex">
+  <div class="bg-blue-600 h-[300px] flex">
     <img
       src="@/assets/logo-bogor.png"
       class="w-45 h-50 opacity-60 m-20"
