0ad9bd09ab9121dc82d019b7e657c960bcfd221f
zurrufi <144505889+zurrufi@users.noreply.github.com>
2023-10-29 12:19:45 +0700

    Update AboutView.vue
    add struktur perangkat

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/STRUKTUR ORGANISASI PKK_page-0001.jpg" alt="" />
+      <img src="@/assets/STRUKTUR_PERANGKAT.jpg" alt="" />
     </div>
   </main>
 </template>
