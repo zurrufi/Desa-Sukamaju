b9954a2da8348fd671827909124bf1c8906be77c
zurrufi <144505889+zurrufi@users.noreply.github.com>
2023-11-02 11:37:01 +0700

    Update Banner.vue
    add new video

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -16,7 +16,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/bOSn2xs__P8?si=PhoQmtKdhr1BIBgy"
+        src="https://www.youtube.com/embed/WBxXO_EsIjs?si=nxylBWLYHtB4IJop"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
